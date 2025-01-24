84f3edde14eaa1d419637cc5f069e3befd50074d
jeffjohnson18 <112980180+jeffjohnson18@users.noreply.github.com>
2025-01-24 15:21:44 -0700

    Update roleassignment.jsx

--- app/roleassignment.jsx
+++ app/roleassignment.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router'; // Use this hook for navigation
+import { useRouter } from 'expo-router';
 
 const RoleAssignmentScreen = ({ route }) => {
 
